e23b85393e94b53248310b711e3e3a21e3d1acfe
olanetsoft <heedris2olubisi@gmail.com>
2020-01-23 16:08:49 +0100

    SITE FOOTER FIXED

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+let date = new Date().toLocaleDateString();
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/olanetsoft"
+                className="icon fa-twitter"
+                target='_blank'
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/olanetsoft"
+                className="icon fa-github"
+                target='_blank'
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://codebushi.com"
+               className="icon fa-facebook"
+               target='_blank'>
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com"
+               className="icon fa-instagram"
+               target='_blank'>
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/olubisi-idris-ayinde-05727b17a/"
+                className="icon fa-linkedin"
+                target='_blank'
+              >
+                <span className="label">linkedIn</span>
+              </a>
+            </li>
+            
+          </ul>
+        <p className="copyright">&copy;{date} Made with <a href="https://www.twitter.com/olanetsoft" className="icon fa-heart"> by @olanetsoft  Powered by</a><a href="https://www.gatsbyjs.org/" > Gatsby</a></p>
     </footer>
 )
 
