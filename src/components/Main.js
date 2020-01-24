91c6c32cb15bbef21848249659f97204dee09686
olanetsoft <heedris2olubisi@gmail.com>
2020-01-24 19:09:32 +0100

    FORM RESTORED

--- src/components/Main.js
+++ src/components/Main.js
@@ -367,10 +367,7 @@ class Main extends React.Component {
           <h2 className="major">Contact</h2>
           <form 
           method="post" 
-          action="#" 
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          >
+          action="#" >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
