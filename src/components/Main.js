325a5f344e8adc57beccca56609e71c0fcc54133
olanetsoft <heedris2olubisi@gmail.com>
2020-01-24 19:21:14 +0100

    FORM RESTORED AGAIN

--- src/components/Main.js
+++ src/components/Main.js
@@ -365,12 +365,9 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact"
-           method="POST" 
-           data-netlify="true"
-          // method="post" 
-          // action="#" 
-          >
+          <form 
+          method="post" 
+          action="#" >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
