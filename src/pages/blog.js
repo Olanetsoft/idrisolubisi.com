67bded19f7aa1a5ba775168caa30b9f2553de897
olanetsoft <heedris2olubisi@gmail.com>
2020-01-26 18:41:17 +0100

    BLOG PAGE update

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Blog;
