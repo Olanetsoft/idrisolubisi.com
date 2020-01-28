d3023773e41236d5b62394e551f69ff5ecddbf4e
olanetsoft <heedris2olubisi@gmail.com>
2020-01-28 09:23:33 +0100

    GATSBY MANIFEST DETAILS UPDATED

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Idris Olubisi Portfolio',
+        short_name: 'Blog',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,51 +5,6 @@ let date = new Date();
 let newDate = date.getFullYear();
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        {/* <ul className="icons">
-            <li>
-              <a
-                href="https://twitter.com/olanetsoft"
-                className="icon fa-twitter"
-                target='_blank'
-              >
-                <span className="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/olanetsoft"
-                className="icon fa-github"
-                target='_blank'
-              >
-                <span className="label">GitHub</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com"
-               className="icon fa-facebook"
-               target='_blank'>
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://instagram.com"
-               className="icon fa-instagram"
-               target='_blank'>
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/olubisi-idris-ayinde-05727b17a/"
-                className="icon fa-linkedin"
-                target='_blank'
-              >
-                <span className="label">linkedIn</span>
-              </a>
-            </li>
-            
-          </ul>
-         */}
          <ul className="icons">
             <li>
               <a
