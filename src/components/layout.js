500659113bf042541a07e2b83b699aa571aa085f
olanetsoft <heedris2olubisi@gmail.com>
2020-01-30 20:53:36 +0100

    ADDED AVIEW PORT

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,7 +41,7 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
+              { name: "viewport", content: "width=device-width, initial-scale=1" },
               { name: 'keywords', content: 'sample, something' },
             ]}
           >
