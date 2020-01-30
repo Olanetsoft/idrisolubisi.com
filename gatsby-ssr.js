df6adb86e11ee592455344d8ce4b46531a65de2e
olanetsoft <heedris2olubisi@gmail.com>
2020-01-30 10:42:13 +0100

    REMOVED SCRIPT TO PRECONNET IN THE GATSBY-SSR.JS

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,14 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="dns-prefetch"
-      key="dns-prefetch-google-analytics"
-      href="https://www.google-analytics.com"
-    />,
-  ]);
-};
