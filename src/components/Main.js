19acfb61a786ce71ef05dabc73dde6249e3ea311
olanetsoft <heedris2olubisi@gmail.com>
2020-08-08 00:14:54 +0100

    trying to fix failed build

--- src/components/Main.js
+++ src/components/Main.js
@@ -66,11 +66,7 @@ class Main extends React.Component {
             libero eu nibh porttitor fermentum. Nullam venenatis erat id
             vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
             Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            dolor.
           </p>
           {close}
         </article> */}
