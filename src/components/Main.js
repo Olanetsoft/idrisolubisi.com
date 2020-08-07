1d7943b37cb5e74e8a77c2cdfb6642b439ecc8f6
olanetsoft <heedris2olubisi@gmail.com>
2020-08-08 00:07:25 +0100

    FIxed broken images

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ class Main extends React.Component {
 
   render() {
     let myFunction = () => {
-      alert("Thanks For getting in Touch... You will get a feedback soon")
+      alert("Thanks For getting in Touch... You will get a feedback soon !")
     }
     let close = (
       <div
