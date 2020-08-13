b6dbf684c9c44575d024b26ffc60234c68763a82
olanetsoft <heedris2olubisi@gmail.com>
2020-08-13 14:20:01 +0100

    base seo image removed for post

--- src/components/seo.js
+++ src/components/seo.js
@@ -44,10 +44,10 @@ function SEO({ description, lang, meta, title, slug, siteImage }){
                property: `og:type`,
                content: `website`,
              },
-             {
-               property: `og:image`,
-               content: `${site.siteMetadata.siteImage}`,
-             },
+            //  {
+            //    property: `og:image`,
+            //    content: `${site.siteMetadata.siteImage}`,
+            //  },
              {
                name: `twitter:card`,
                content: `summary_large_image`,
